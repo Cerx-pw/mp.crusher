8f2f8951e4c88a46f3ee15db1a150c86cac36a0b
Vojtěch Ducháček <cerx@cerx.pw>
2016-09-11 00:06:56 +0200

    Fixed progress visualization bug

--- app.js
+++ app.js
@@ -2,10 +2,14 @@ var request = require('request');
 console.log("Initializing...")
 var slash = 0
 function ping(){
-    console.log('\033[2J');
     request('http://glowcraft.fakaheda.eu/podpis/podpis.php', function (error,response,body) {
+        process.stdout.write('\033c');
         if(slash == 1){
             console.log("\\...");
+            slash = 2;
+        }
+        else if(slash == 2){
+            console.log("|...");
             slash = 0;
         }
         else{
