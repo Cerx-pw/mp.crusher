7cda513932c0cba3b9a22408fc7e379b0c24e790
Vojtěch Ducháček <cerx@cerx.pw>
2016-09-11 00:02:53 +0200

    Added progress visualization

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 var request = require('request');
+console.log("Initializing...")
+var slash = 0
 function ping(){
+    console.log('\033[2J');
     request('http://glowcraft.fakaheda.eu/podpis/podpis.php', function (error,response,body) {
+        if(slash == 1){
+            console.log("\\...");
+            slash = 0;
+        }
+        else{
+            console.log("/...");
+            slash = 1;
+        }
         setTimeout(ping, 1);
     });
 }
